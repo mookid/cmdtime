131b6c637ee3428ff859fef48e56de52fda08d23
Nathan Moreau <nathan.moreau@m4x.org>
2020-07-30 18:59:12 +0200

    CLI: remove dependency on clap; allow long and short flags.

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,61 @@
 use winapi::shared::minwindef::BOOL;
 use winapi::shared::minwindef::FALSE;
 use winapi::shared::minwindef::TRUE;
-use winapi::shared::ntdef::*;
-use winapi::um::winbase::*;
-use winapi::um::winnt::JOBOBJECT_ASSOCIATE_COMPLETION_PORT;
-use winapi::um::winnt::JOB_OBJECT_MSG_ACTIVE_PROCESS_ZERO;
-
-use clap::{App, AppSettings, Arg};
+use winapi::shared::ntdef::HANDLE;
+use winapi::shared::ntdef::NULL;
+use winapi::shared::ntdef::PVOID;
 
 use std::ffi::OsStr;
 use std::iter::once;
+use std::iter::Peekable;
 use std::os::windows::ffi::OsStrExt;
-
 use std::ptr::null_mut;
 
-const OPT_COMMAND: &str = "command";
-const OPT_OUTPUTFILE: &str = "output";
-const FLAG_APPEND: &str = "append";
+const USAGE: &str = r#"Usage: $BIN_NAME [-aV] [-o file] [--append] [--output file]
+       [--help] [--version] command [arg...]"#;
+const BIN_NAME: &str = env!("CARGO_PKG_NAME");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+const EXIT_ERROR: i32 = 2;
 
-struct Wrapper<T>(T);
+fn ignore<T>(_: T) {}
+
+fn usage(code: i32) -> ! {
+    eprintln!("{}", USAGE.replace("$BIN_NAME", BIN_NAME));
+    std::process::exit(code);
+}
+
+fn invalid_opt(arg: impl std::fmt::Display) -> ! {
+    eprintln!("invalid option: '{}'", arg);
+    usage(EXIT_ERROR);
+}
+
+fn missing_arg(arg: impl std::fmt::Display) -> ! {
+    eprintln!("option requires an argument: '{}'", arg);
+    usage(EXIT_ERROR);
+}
+
+fn show_version() -> ! {
+    eprintln!("{} {}", BIN_NAME, VERSION);
+    std::process::exit(0);
+}
 
 fn win32_assert(result: BOOL, caller_name: &'static str) {
     if result == FALSE {
         eprintln!("{}: {}", caller_name, std::io::Error::last_os_error());
-        std::process::exit(2);
+        std::process::exit(EXIT_ERROR);
     }
 }
 
 fn win32_assert_not_null(result: PVOID, caller_name: &'static str) {
     if result == NULL {
         eprintln!("{}: {}", caller_name, std::io::Error::last_os_error());
-        std::process::exit(2);
+        std::process::exit(EXIT_ERROR);
     }
+}
+
+fn die_io_error(msg: &'static str, e: std::io::Error) -> ! {
+    eprintln!("{}: {}", msg, e);
+    std::process::exit(EXIT_ERROR);
 }
 
 struct ProcessDescr(HANDLE);
@@ -48,9 +72,7 @@ struct ThreadDescr(HANDLE);
 impl ThreadDescr {
     fn resume(&self) {
         use winapi::um::processthreadsapi::ResumeThread;
-        unsafe {
-            ResumeThread(self.0);
-        }
+        unsafe { ResumeThread(self.0) };
     }
 }
 
@@ -86,33 +108,22 @@ fn void_ptr<T, R>(value: &mut T) -> *mut R {
     ptr(value) as _
 }
 
-fn get_perf_counter() -> f64 {
+fn win32_get_perf_counter() -> f64 {
     use winapi::um::profileapi::QueryPerformanceCounter;
 
-    let mut w = Wrapper(0.0);
-    let ret = unsafe { QueryPerformanceCounter(void_ptr(&mut w)) };
-    if ret == FALSE {
-        eprintln!(
-            "wipapi QueryPerformanceCounter: {}",
-            std::io::Error::last_os_error()
-        );
-        std::process::exit(2);
-    }
-    w.0
+    let mut res = 0.0;
+    let ret = unsafe { QueryPerformanceCounter(void_ptr(&mut res)) };
+    win32_assert(ret, "QueryPerformanceFrequency");
+    res
 }
 
-fn get_perf_freq() -> f64 {
+fn win32_get_perf_freq() -> f64 {
     use winapi::um::profileapi::QueryPerformanceFrequency;
 
-    let mut w = Wrapper(0.0);
-    let ret = unsafe { QueryPerformanceFrequency(void_ptr(&mut w)) };
-    if ret == FALSE {
-        eprintln!(
-            "wipapi QueryPerformanceFrequency: {}",
-            std::io::Error::last_os_error()
-        )
-    }
-    w.0
+    let mut res = 0.0;
+    let ret = unsafe { QueryPerformanceFrequency(void_ptr(&mut res)) };
+    win32_assert(ret, "QueryPerformanceFrequency");
+    res
 }
 
 fn convert_utf16(s: &str) -> Vec<u16> {
@@ -120,13 +131,14 @@ fn convert_utf16(s: &str) -> Vec<u16> {
 }
 
 fn win32_create_suspended_process(cmd: &str) -> (ProcessDescr, ThreadDescr) {
-    use winapi::um::processthreadsapi::*;
+    use winapi::um::processthreadsapi::CreateProcessW;
+    use winapi::um::winbase::CREATE_SUSPENDED;
 
     let mut command_line = convert_utf16(cmd);
 
     let (hthread, hprocess);
     unsafe {
-        let mut startup_info = _0(); // TODO redirect stdin/stdout/stderr
+        let mut startup_info = _0();
         let mut process_info = _0();
         let res = CreateProcessW(
             /* lpApplicationName    */ null_mut(),
@@ -154,6 +166,7 @@ fn win32_create_job() -> JobDescr {
     use winapi::um::jobapi2::CreateJobObjectW;
     use winapi::um::jobapi2::SetInformationJobObject;
     use winapi::um::winnt::JobObjectAssociateCompletionPortInformation;
+    use winapi::um::winnt::JOBOBJECT_ASSOCIATE_COMPLETION_PORT;
 
     let (hjob, hiocp);
     unsafe {
@@ -179,15 +192,15 @@ fn win32_create_job() -> JobDescr {
 
 fn win32_attach_process_to_job(process: &ProcessDescr, job: &JobDescr) {
     use winapi::um::jobapi2::AssignProcessToJobObject;
-    unsafe {
-        let res = AssignProcessToJobObject(job.hjob, process.0);
-        win32_assert(res, "AssignProcessToJobObject");
-    }
+    let res = unsafe { AssignProcessToJobObject(job.hjob, process.0) };
+    win32_assert(res, "AssignProcessToJobObject");
 }
 
 impl JobDescr {
     fn wait_for_job_completion(&self) {
         use winapi::um::ioapiset::GetQueuedCompletionStatus;
+        use winapi::um::winbase::INFINITE;
+        use winapi::um::winnt::JOB_OBJECT_MSG_ACTIVE_PROCESS_ZERO;
 
         unsafe {
             let mut completion_code = _0();
@@ -208,8 +221,8 @@ impl JobDescr {
 
     fn get_job_times(&self) -> Times {
         use winapi::um::jobapi2::QueryInformationJobObject;
-        use winapi::um::winnt::JOBOBJECT_BASIC_ACCOUNTING_INFORMATION;
         use winapi::um::winnt::JobObjectBasicAccountingInformation;
+        use winapi::um::winnt::JOBOBJECT_BASIC_ACCOUNTING_INFORMATION;
         use winapi::um::winnt::LARGE_INTEGER;
 
         fn to_seconds(value: LARGE_INTEGER) -> f64 {
@@ -233,49 +246,13 @@ impl JobDescr {
             kernel: to_seconds(info.TotalKernelTime),
         }
     }
-
 }
 
-fn app() -> App<'static, 'static> {
-    App::new("cmdtime")
-        .setting(AppSettings::UnifiedHelpMessage)
-        .setting(AppSettings::TrailingVarArg)
-        .setting(AppSettings::DontCollapseArgsInUsage)
-        .setting(AppSettings::DontDelimitTrailingValues)
-        .version("0.1.0")
-        .usage("cmdtime -- command [arg...]")
-        .author("Nathan Moreau <nathan.moreau@m4x.org>")
-        .arg(
-            Arg::with_name(OPT_COMMAND)
-                .takes_value(true)
-                .required(true)
-                .multiple(true)
-                .min_values(1)
-                .help("The command to launch")
-                .last(true),
-        )
-        .arg(
-            Arg::with_name(OPT_OUTPUTFILE)
-                .takes_value(true)
-                .short("-o")
-                .help("The file to write output to, default to stderr"),
-        )
-        .arg(
-            Arg::with_name(FLAG_APPEND)
-                .short("-a")
-                .help("If true, append rather than truncate the outputfile"),
-        )
-}
-
-fn print_duration(
-    f: &mut impl std::io::Write,
-    name: &'static str,
-    seconds: f64,
-) {
+fn print_duration(f: &mut impl std::io::Write, name: &'static str, seconds: f64) {
     let minutes = seconds.floor() as i64 / 60;
     let seconds = seconds - 60.0 * minutes as f64;
     if let Err(e) = writeln!(f, "{}\t{}m{:.3}s", name, minutes, seconds) {
-        eprintln!("failed to write: {}", e);
+        die_io_error("failed to write", e);
     }
 }
 
@@ -288,48 +265,97 @@ fn open_file(path: &std::path::Path, append: bool) -> std::io::Result<std::fs::F
         .open(path)
 }
 
-fn main() {
-    let matches = app().get_matches();
-    let freq = get_perf_freq();
+#[derive(Default)]
+struct Opts {
+    ofile: Option<String>,
+    append: bool,
+}
 
-    let mut w: Box<dyn std::io::Write> = if let Some(ofile) = matches.value_of_os(OPT_OUTPUTFILE) {
-        match open_file(
-            std::path::Path::new(ofile),
-            matches.is_present(FLAG_APPEND),
-        ) {
-            Ok(fd) => Box::new(fd),
-            Err(e) => {
-                eprintln!("failed to open file: {}", e);
-                std::process::exit(2);
+fn parse_output(opts: &mut Opts, args: &mut Peekable<impl Iterator<Item = String>>) -> bool {
+    let arg = args.next().unwrap();
+    match args.next() {
+        None => {
+            missing_arg(arg);
+        }
+        Some(f) => {
+            opts.ofile = Some(f);
+            true
+        }
+    }
+}
+
+fn parse_append(opts: &mut Opts, args: &mut Peekable<impl Iterator<Item = String>>) -> bool {
+    args.next();
+    opts.append = true;
+    true
+}
+
+fn parse_arg(opts: &mut Opts, args: &mut Peekable<impl Iterator<Item = String>>) -> bool {
+    if let Some(arg) = args.peek() {
+        let arg = arg.clone();
+        if arg.starts_with("--") {
+            match &*arg {
+                "--output" => parse_output(opts, args),
+                "--help" => usage(0),
+                "--version" => show_version(),
+                "--append" => parse_append(opts, args),
+                arg => invalid_opt(arg),
             }
+        } else if arg.starts_with("-") {
+            if arg == "-o" {
+                parse_output(opts, args)
+            } else {
+                for ch in arg.chars().skip(1) {
+                    match ch {
+                        'V' => show_version(),
+                        'a' => ignore(parse_append(opts, args)),
+                        ch => invalid_opt(ch),
+                    }
+                }
+                true
+            }
+        } else {
+            false
         }
     } else {
-        Box::new(std::io::stderr())
+        false
+    }
+}
+
+fn main() {
+    let mut opts = Default::default();
+    let mut args = std::env::args().skip(1).peekable();
+    while parse_arg(&mut opts, &mut args) {}
+    let mut w: Box<dyn std::io::Write> = match opts.ofile {
+        Some(ofile) => match open_file(std::path::Path::new(&ofile), opts.append) {
+            Ok(fd) => Box::new(fd),
+            Err(e) => die_io_error("failed to open file", e),
+        },
+        None => Box::new(std::io::stderr()),
     };
 
-    if let Some(args) = matches.values_of_os(OPT_COMMAND) {
-        let job = win32_create_job();
-
-        let args: Vec<_> = args
-            .map(|arg| arg.to_os_string().into_string().unwrap())
-            .collect();
-        let args = args.join(" ");
-        let (process, thread) = win32_create_suspended_process(&args);
-        win32_attach_process_to_job(&process, &job);
-        drop(process);
-
-        let wall0 = get_perf_counter();
-        thread.resume();
-        drop(thread);
-        job.wait_for_job_completion();
-        let wall1 = get_perf_counter();
-
-        let wall = (wall1 - wall0) / freq;
-
-        let job_times = job.get_job_times();
-
-        print_duration(&mut w, "real", wall);
-        print_duration(&mut w, "user", job_times.user);
-        print_duration(&mut w, "sys", job_times.kernel);
+    let args: Vec<_> = args.collect();
+    if args.is_empty() {
+        usage(EXIT_ERROR)
     }
+    let args = args.join(" ");
+    let freq = win32_get_perf_freq();
+    let job = win32_create_job();
+    let (process, thread) = win32_create_suspended_process(&args);
+    win32_attach_process_to_job(&process, &job);
+    drop(process);
+
+    let wall0 = win32_get_perf_counter();
+    thread.resume();
+    drop(thread);
+    job.wait_for_job_completion();
+    let wall1 = win32_get_perf_counter();
+
+    let wall = (wall1 - wall0) / freq;
+
+    let job_times = job.get_job_times();
+
+    print_duration(&mut w, "real", wall);
+    print_duration(&mut w, "user", job_times.user);
+    print_duration(&mut w, "sys", job_times.kernel);
 }
